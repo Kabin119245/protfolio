ffd1147bd4186e217fb2ebdd73da1c3d222c0bf7
Kabin119245 <85396529+Kabin119245@users.noreply.github.com>
2023-08-06 22:50:37 +0545

    new version

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const logo = document.getElementById("logo");
+    const homePageURL = "./index.html";
+
+    logo.addEventListener("click", function () {
+      window.location.href = homePageURL;
+    });
+  });
+
+
+
 function toggleMenu() {
   const navbar = document.querySelector(".navbar");
   navbar.classList.toggle("active");
 }
-console.log(navbar)
+
+var counter = 1;
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if (counter>4) {
+    counter = 1;
+  }
+},5000);
